3a4d1f652dcc431e97828c9a011e99c95f2db9ef
William Madden <madden.william.d@gmail.com>
2016-12-06 14:11:29 -0800

    checnges to algorythm to accomodate Meghans improvement on my originally designed js flow.

--- wills/pageTwoScript.js
+++ wills/pageTwoScript.js
@@ -4,10 +4,18 @@
  a. check fo in storage
  b. get from stroage
  c. parse, read and record
- d. identify appropriate template
-2 prompt, collect, store user data
- a.
- b.
- c.
+2. set page style
+  a. check genre choice
+  b. match with appropriate style class
+  c. aply style class to all elements
+3 prompt, collect, store user data
+//prompt for user name and email, establish genre specific visual theme
+ a. indentify and capture first form element DOM node
+ b. change element property from hidden. //reveals fisrt form
+  >>>form one<<<
+   I. Collect record and save user data
+   II. Identify input elements and capture, add event listener<<<streth goaql of adding ie8 fix for absence of addeventlistner method
+   III. 1
+ c. update element on page, effectivly displaying the promp
 
 */
