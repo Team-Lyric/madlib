b0e710b2e9a1c94863b11079f15d307286a296d4
William Madden <madden.william.d@gmail.com>
2016-12-07 13:37:59 -0800

    getting maras contributions

--- wills/PageOneScriptFunctions.js
+++ wills/PageOneScriptFunctions.js
@@ -14,84 +14,90 @@ contentArea.addEventListner(click, clickHandler());
 
 /**///here is a handler that will use the mouse position
 //to determin which template the user selects
-function clickHandler(event){
-//collect mouse location
-  function doSomething(e) {
-    var posx = 0;
-    var posy = 0;
-    if (!e) /*var*/ e = window.event; //original code edited
-    if (e.pageX || e.pageY) 	{
-      posx = e.pageX;
-      posy = e.pageY;
-    }
-    else if (e.clientX || e.clientY) 	{
-      posx = e.clientX + document.body.scrollLeft
-      + document.documentElement.scrollLeft;
-      posy = e.clientY + document.body.scrollTop
-      + document.documentElement.scrollTop;
-    }
-    return[posx, posy ];
-    /*CITE >>>>>http://www.quirksmode.org/js/events_properties.html
-    QuirksMode.org is the prime source for browser compatibility
-    information on the Internet. It is maintained by Peter-Paul Koch,
-     mobile platform strategist in Amsterdam, the Netherlands.*/
-  }
-  var mouseLocation = doSomething();
-  var x = mouseLocation[0];
-  var y = mouseLocation[1];
-  var templateSelection;
-  if (x /*x is grater than halfway accross the width of the contentArea*/ ) {
-
-  //the right half of contentArea, (pop, country)
-    if(false/*y is greater than halfway down the height of contentArea*/){
-    //lower right corner
-      templateSelection = 'country';
-    }
-    else {
-      //upper right corner
-      templateSelection = 'pop';
-    }
-  }
-  else{
-  //the left half of contentArea
-    if(y/*y is greater than halfway down the height of contentArea*/) {
-    //lower left corner
-      templateSelection = 'r&b';
-    }
-    else {
-    //upper left corner
-      templateSelection = 'rock';
-    }
-  }
-  var genreChoice = JSON.stringify(templateSelection);
-  localStorage.setItem(genreChoice, genreChoice);
-}
+// function clickHandler(event){
+// //collect mouse location
+//   function doSomething(e) {
+//     var posx = 0;
+//     var posy = 0;
+//     if (!e) /*var*/ e = window.event; //original code edited
+//     if (e.pageX || e.pageY) 	{
+//       posx = e.pageX;
+//       posy = e.pageY;
+//     }
+//     else if (e.clientX || e.clientY) 	{
+//       posx = e.clientX + document.body.scrollLeft
+//       + document.documentElement.scrollLeft;
+//       posy = e.clientY + document.body.scrollTop
+//       + document.documentElement.scrollTop;
+//     }
+//     return[posx, posy ];
+//     /*CITE >>>>>http://www.quirksmode.org/js/events_properties.html
+//     QuirksMode.org is the prime source for browser compatibility
+//     information on the Internet. It is maintained by Peter-Paul Koch,
+//      mobile platform strategist in Amsterdam, the Netherlands.*/
+//   }
+//   var mouseLocation = doSomething();
+//   var x = mouseLocation[0];
+//   var y = mouseLocation[1];
+//   var templateSelection;
+//   if (x /*x is grater than halfway accross the width of the contentArea*/ ) {
+//
+//   //the right half of contentArea, (pop, country)
+//     if(false/*y is greater than halfway down the height of contentArea*/){
+//     //lower right corner
+//       templateSelection = 'country';
+//     }
+//     else {
+//       //upper right corner
+//       templateSelection = 'pop';
+//     }
+//   }
+//   else{
+//   //the left half of contentArea
+//     if(y/*y is greater than halfway down the height of contentArea*/) {
+//     //lower left corner
+//       templateSelection = 'r&b';
+//     }
+//     else {
+//     //upper left corner
+//       templateSelection = 'rock';
+//     }
+//   }
+//   var genreChoice = JSON.stringify(templateSelection);
+//   localStorage.setItem(genreChoice, genreChoice);
+// }
 
 
 /**///here is a handler that will use a grid of four
 //divs and determin which template the user selects
 // based on which div the event is fired from
-function clickHandler2(event){
+function clickHandler(event){
 //collect mouse location
   var divChoice = event.target.id;
   var templateSelection;
-  if (divChoice /* div choice is one*/ ) {
+  var genres = ['rock', 'pop', 'randb', 'country'];
+
+
+
+  if (divChoice === genres[0] ) {
       //upper left
     templateSelection = 'rock';
   }
-  else if (false /* div choice is two */){
+  else if (divChoice === genres[1]){
       //upper right corner
     templateSelection = 'pop';
   }
-  else if (false /*div choice is three*/){
+  else if (divChoice === genres[2]){
       //lower left corner
-    templateSelection = 'r&b';
+    templateSelection = 'rNb';
   }
-  else {
+  else if (divChoice === genres[3]){
       //lower right corner
     templateSelection = 'country';
   }
-
+  else{
+    alert('choose a genre of music');
+  }
   var genreChoice = JSON.stringify(templateSelection);
   localStorage.setItem(genreChoice, genreChoice);
 }
