efc91a3afb67525c88472e5e7aac50902bd92bc5
meghanroserebecca <meghanroserebecca@gmail.com>
2016-12-07 17:56:31 -0800

    have created and tested rockTemplate1 object -- good to go

--- meghanAppType/app.js
+++ meghanAppType/app.js
@@ -10,26 +10,7 @@ function Template ( title, template, needed, input, lyricLib){
   this.lyricLib = lyricLib;
 }
 
-var rockTemplate1 = new Template (/*title*/'Welcome to the Jungle',/*template*/'Welcome to the ' + '***Natural Place***' + 'we got fun and games \n
-We got everything you want' + '***Sweet Substance***' + ', we know the names \n
-We are the ' + '***Plural Noun***' + ' that can ' + '***Verb***' + ' whatever you may need \n
-If you got the ' + '***Noun***' + ' honey we got your ' + '***Disease***' + '\n \n
-In the ' + '***Natural Place***' + ', welcome to the ' + '***Natural Place***' + '\n
-Watch it bring you to your sha na na na na ' + '***Body Part (Plural)***' + \n
-I wanna watch you bleed \n \n
-Welcome to the + ***Natural Place#*** +  we take it day by day \n
-If you want it you're gonna bleed but it's the price you pay \n
-And you\'re a very + ***Adjective*** + ***Noun*** + very + ***Adjective*** + to please \n
-You can taste the + ***Adjective*** + ***Plural Noun*** + but you won't get there for free \n
-In the jungle welcome to the + ***Natural Place*** + \n
-Feel my, my, my, my serpentine \n
-I,I wanna hear you + ***Verb*** + \n \n
-Welcome to the + ***Natural Place*** + it gets worse here everyday \n
-You learn to live like an + ***Noun*** + in the + ***Natural Place*** + where we + ***Verb*** + \n
-If you got a hunger for what you see you'll + ***Verb*** + it eventually \n
-You can have everything you want but you better not take it from me \n
-In the + ***Natural Place***+, welcome to the + ***Natural Place*** + \n
-Watch it bring you to your sha na na na na + ***Body Part (Plural)*** + \n
-I'm gonna watch you + ***Verb*** + \n \n
-And when you're high you never ever want to come down \n
-So down, so down, so down, yeah \n \n
+var rockTemplate1 = new Template (/*title*/'Welcome to the Jungle',/*template*/'Welcome to the ' + '***Natural Place***' + ' we got fun and games \n We got everything you want' + '***Sweet Substance***' + ', we know the names \n We are the ' + '***Plural Noun***' + ' that can ' + '***Verb***' + ' whatever you may need \n If you got the ' + '***Noun***' + ' honey we got your ' + '***Disease***' + '\n In the ' + '***Natural Place***' + ', welcome to the ' + '***Natural Place***' + '\n Watch it bring you to your sha na na na na ' + '***Body-Part (Plural)***' + '\n I wanna watch you bleed \n \n Welcome to the ' + '***Natural Place#***' + ' we take it day by day \n If you want it you\'re gonna bleed but it\'s the price you pay \n And you\'re a very ' + '***Adjective***' + '' + '***Noun***' + ' very ' + '***Adjective***' + ' to please \n You can taste the ' + '***Adjective***' + '***Plural Noun***' + 'but you won\'t get there for free \n In the jungle welcome to the ' + '***Natural Place***' + '\n Feel my, my, my, my serpentine \n I,I wanna hear you ' + '***Verb***' + '\n \n Welcome to the ' + '***Natural Place***' + ' it gets worse here everyday \n You learn to live like an ' + '***Noun***' + 'in the ' + '***Natural Place***' + ' where we' + '***Verb***' + '\n If you got a hunger for what you see you\'ll' + '***Verb***' + 'it eventually \n You can have everything you want but you better not take it from me \n In the ' + '***Natural Place***' + ', welcome to the ' + '***Natural Place***' + '\n Watch it bring you to your sha na na na na ' + '***Body Part (Plural)***' + '\n I\'m gonna watch you bleed \n \n And when you\'re high you never ever want to come down \n So down, so down, so down, yeah \n',
+['Natural Place', 'Sweet Substance', 'Plural Noun', 'Verb', 'Noun', 'Disease', 'Natural Place', 'Natural Place', 'Body-Part (Plural)', 'Natural Place', 'Adjective', 'Noun', 'Adjective', 'Adjective', 'Plural Noun', 'Natural Place', 'Verb', 'Natural Place', 'Noun', 'Natural Place', 'Verb', 'Verb', 'Natural Place', 'Natural Place', 'Body-Part (Plural)'],
+[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y],
+'Welcome to the ' + a + ' we got fun and games \n We got everything you want' + b + ', we know the names \n We are the ' + c + ' that can ' + d + ' whatever you may need \n If you got the ' + e + ' honey we got your ' + f + '\n In the ' + g + ', welcome to the ' + h + '\n Watch it bring you to your sha na na na na ' + i + '\n I wanna watch you bleed \n \n Welcome to the ' + j + ' we take it day by day \n If you want it you\'re gonna bleed but it\'s the price you pay \n And you\'re a very ' + k + '' + l + ' very ' + m + ' to please \n You can taste the ' + n + '' + o + 'but you won\'t get there for free \n In the jungle welcome to the ' + p + '\n Feel my, my, my, my serpentine \n I,I wanna hear you ' + q + '\n \n Welcome to the ' + r + ' it gets worse here everyday \n You learn to live like an ' + s + 'in the ' + t + ' where we' + u + '\n If you got a hunger for what you see you\'ll' + v + 'it eventually \n You can have everything you want but you better not take it from me \n In the ' + w + ', welcome to the ' + x + '\n Watch it bring you to your sha na na na na ' + y + '\n I\'m gonna watch you bleed \n \n And when you\'re high you never ever want to come down \n So down, so down, so down, yeah \n')
