0e2b98dfb022ae320ab0a34ebbba8575a986ba79
meghanroserebecca <meghanroserebecca@gmail.com>
2016-12-09 00:33:57 -0800

    jesus harold cristo i think i finally got it

--- app1.js
+++ app1.js
@@ -1,3 +1,7 @@
 'use strict';
 
- /* onclick leads to document.getElementById('game_input').style.visibility='visible';*/
+var rock = document.getElementById("rock_piece");
+
+rock.addEventListener("mouseover", function(event) {
+  event.target.style.opacity = 1;
+}, false);
