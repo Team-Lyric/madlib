400d0bf2a0a28b52ea30fb012a371e497b458672
William Madden <madden.william.d@gmail.com>
2016-12-09 11:59:00 -0800

    placing all like file types together to compare and integrate

---
+++ js/splashpageNavscript.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var rockSplash = document.getElementById('rock_piece');
+var popSplash = document.getElementById('pop_piece');
+var rnbSplash = document.getElementById('rnb_piece');
+var countrySplash = document.getElementById('country_piece');
+
+rockSplash.addEventListener("mouseover", function(event) {
+  event.target.style.opacity = 1;
+}, false);
+
+rockSplash.addEventListener("mouseout", function(event) {
+  event.target.style.opacity = 0;
+}, false);
+
+popSplash.addEventListener("mouseover", function(event) {
+  event.target.style.opacity = 1;
+}, false);
+
+popSplash.addEventListener("mouseout", function(event) {
+  event.target.style.opacity = 0;
+}, false);
+
+rnbSplash.addEventListener("mouseover", function(event) {
+  event.target.style.opacity = 1;
+}, false);
+
+rnbSplash.addEventListener("mouseout", function(event) {
+  event.target.style.opacity = 0;
+}, false);
+
+countrySplash.addEventListener("mouseover", function(event) {
+  event.target.style.opacity = 1;
+}, false);
+
+countrySplash.addEventListener("mouseout", function(event) {
+  event.target.style.opacity = 0;
+}, false);
