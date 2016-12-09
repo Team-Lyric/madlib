0f07ae1ea71de2f34a03a45d22070839a8eb4ca0
meghanroserebecca <meghanroserebecca@gmail.com>
2016-12-09 01:01:44 -0800

    soo sleeepy. got landing page half-working in a jenky sorta way

--- app1.js
+++ app1.js
@@ -1,7 +1,38 @@
 'use strict';
 
 var rock = document.getElementById("rock_piece");
+var pop = document.getElementById("pop_piece");
+var rnb = document.getElementById("'rnb_piece'");
+var country = document.getElementById("country_piece");
 
 rock.addEventListener("mouseover", function(event) {
   event.target.style.opacity = 1;
+}, false);
+
+rock.addEventListener("mouseout", function(event) {
+  event.target.style.opacity = 0;
+}, false);
+
+pop.addEventListener("mouseover", function(event) {
+  event.target.style.opacity = 1;
+}, false);
+
+pop.addEventListener("mouseout", function(event) {
+  event.target.style.opacity = 0;
+}, false);
+
+rnb.addEventListener("mouseover", function(event) {
+  event.target.style.opacity = 1;
+}, false);
+
+rnb.addEventListener("mouseout", function(event) {
+  event.target.style.opacity = 0;
+}, false);
+
+country.addEventListener("mouseover", function(event) {
+  event.target.style.opacity = 1;
+}, false);
+
+country.addEventListener("mouseout", function(event) {
+  event.target.style.opacity = 0;
 }, false);
